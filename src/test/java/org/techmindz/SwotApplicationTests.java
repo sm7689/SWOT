e4a53ea402863d7a2d6e6e0293f6ced7aa08a549
Kiran25-web <kiranvarghese49@gmail.com>
2022-07-12 15:53:30 +0530

    test

---
+++ src/test/java/org/techmindz/SwotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.techmindz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
